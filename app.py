843d8fcc2fb1453f3cd70e8949e7ae1e9be23692
sivapullaiahmangali <sivapullaiahmangali@gmail.com>
2023-06-29 09:52:44 +0530

    test of mogoDB pushing data

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+# Connect to your MongoDB Cloud database
+client = MongoClient('mongodb+srv://mangalisivapullaiah:9391993975@cluster0.tmdwkd0.mongodb.net/?retryWrites=true&w=majority&collection=users')
+# mongodb+srv://<username>:<password>@cluster0.tmdwkd0.mongodb.net/?retryWrites=true&w=majority
+
+# Define the collection
+db = client['your_database_name']
+collection = db['signup_details']
+
+@app.route('/')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    
+    # Insert the signup details into the collection
+    collection.insert_one({'name': name, 'email': email, 'password': password})
+    
+    return 'Signup successful!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
